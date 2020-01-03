f982c1c3d42f95f3051cae9a2320793a28f4c2e4
xingxiaohui <1559237979@qq.com>
2020-01-03 17:28:46 +0800

    实现图片上传预览等功能

---
+++ cloud_img/static/js/index.js
@@ -0,0 +1,47 @@
+layui.use(['layer','upload'], function(){
+    var $ = layui.jquery;
+    var layer = layui.layer;
+    var upload = layui.upload;
+    var preview_url = "";
+
+    //执行实例
+    var uploadInst = upload.render({
+        elem: '#upload' //绑定元素
+        ,url: '/upload/' //上传接口
+        ,accept: 'images' //允许上传的文件类型
+        ,size: 10240 //最大允许上传的文件大小
+        ,done: function(res){
+            // 上传完毕回调
+            if (res.code == 200) {
+                layer.msg(res.msg, {icon: 1});
+                $('#url').val(res.url);  // res.url为上传后的文件url
+                preview_url = '<img src="'+res.url+'">'
+                $('#html-url').val(preview_url);
+                $("#notice").html(res.msg);
+                $("#res-box").show();
+            } else {
+                layer.msg(res.msg, {icon: 2});
+            }
+        },
+        error: function () {  // 请求异常回调
+            layer.msg('上传失败', {icon: 2});
+        }
+    });
+    $("#copy").click(function(){
+        var url = $("#url").select();
+        // 与其说是被禁用，不如说是不想写 哈哈哈哈 ^--^
+        $("#notice").html("您的浏览器禁用了复制功能，请使用Ctrl+C复制！");
+    });
+    $("#html-copy").click(function(){
+        var url = $("#html-url").select();
+        $("#notice").html("您的浏览器禁用了复制功能，请使用Ctrl+C复制！");
+    });
+    $("#preview").click(function(){
+         layer.open({
+             title: '图片预览',
+             offset: '100px',
+             area: ['1000px', '700px'],
+             content: preview_url
+         });
+    });
+});
--- cloud_img/views.py
+++ cloud_img/views.py
@@ -1,7 +1,36 @@
-from flask import render_template
+import os
+import uuid
+
+from flask import render_template, json, request
+from werkzeug.utils import secure_filename
+
 from cloud_img import app
+
+# 设置允许的文件格式
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'JPG', 'PNG', 'bmp'])
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/upload/', methods={'post'})
+def upload():
+    f = request.files['file']
+    if not (f and allowed_file(f.filename)):
+        return json.dumps({"code": 500, "msg": '文件格式有误！'})
+    # 当前文件所在路径
+    base_path = os.path.dirname(__file__)
+    # 注意：没有的文件夹一定要先创建，不然会提示没有该路径
+    path = uuid.uuid4().hex[0:8] + '.' + f.filename.rsplit('.', 1)[1]
+    upload_path = os.path.join(base_path, 'static/image/upload', path)
+    f.save(upload_path)
+    url = 'http://localhost:5000/static/image/upload/' + path
+    return json.dumps({"code": 200,
+                       "msg": '上传成功！',
+                       "url": url})
